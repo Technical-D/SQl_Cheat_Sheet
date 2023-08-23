2059ca4e21e6b5269a4f87ef9f25f99485f485d7
Dheeraj Gupta <dheerajgupta1902@gmail.com>
2023-08-23 17:12:38 +0530

    'third'

--- statements/aggregate_functions.sql
+++ statements/aggregate_functions.sql
@@ -102,3 +102,5 @@ FROM (SELECT total_amt_usd
          LIMIT 3457) AS Table1
 ORDER BY total_amt_usd DESC
 LIMIT 2;
+-- Since there are 6912 orders - we want the average of the 3457 and 3456 order amounts when ordered. This is the average of 2483.16 and 2482.55. This gives the median of 2482.855. This obviously isn't an ideal way to compute. If we obtain new orders, we would have to change the limit. SQL didn't even calculate the median for us. The above used a SUBQUERY, but you could use any method to find the two necessary values, and then you just need the average of them.
+
