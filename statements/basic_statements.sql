343a89b919f16151e5769592990f3d65e57dd38d
Dheeraj Gupta <dheerajgupta1902@gmail.com>
2024-12-27 16:56:01 +0530

    changes

--- statements/basic_statements.sql
+++ statements/basic_statements.sql
@@ -241,17 +241,17 @@ AND primary_poc NOT LIKE '%ena%');
 
 Statement	    How to Use It	            Other Details
 
-SELECT	        SELECT Col1, Col2, ...	    Provide the columns you want
+SELECT	        SELECT Col1, Col2, ...	  Provide the columns you want
 FROM	        FROM Table	                Provide the table where the columns exist
 LIMIT	        LIMIT 10	                Limits based number of rows returned
-ORDER BY        ORDER BY Col	            Orders table based on the column. Used with DESC.
-WHERE	        WHERE Col > 5	            A conditional statement to filter your results
-LIKE	        WHERE Col LIKE '%me%'	    Only pulls rows where column has 'me' within the text
-IN	            WHERE Col IN ('Y', 'N')	    A filter for only rows with column of 'Y' or 'N'
-NOT	            WHERE Col NOT IN ('Y', 'N')	NOT is frequently used with LIKE and IN
-AND	            WHERE Col1 > 5 AND Col2 < 3	Filter rows where two or more conditions must be true
-OR	            WHERE Col1 > 5 OR Col2 < 3	Filter rows where at least one condition must be true
-BETWEEN	        WHERE Col BETWEEN 3 AND 5	Often easier syntax than using an AND
+ORDER BY       ORDER BY Col	                Orders table based on the column. Used with DESC.
+WHERE	        WHERE Col > 5	         A conditional statement to filter your results
+LIKE	        WHERE Col LIKE '%me%'	  Only pulls rows where column has 'me' within the text
+IN	        WHERE Col IN ('Y', 'N')	  A filter for only rows with column of 'Y' or 'N'
+NOT	        WHERE Col NOT IN ('Y', 'N')  NOT is frequently used with LIKE and IN
+AND	        WHERE Col1 > 5 AND Col2 < 3  Filter rows where two or more conditions must be true
+OR	        WHERE Col1 > 5 OR Col2 < 3	  Filter rows where at least one condition must be true
+BETWEEN	 WHERE Col BETWEEN 3 AND 5	  Often easier syntax than using an AND
 
 -- Other Tips
 -- Though SQL is not case sensitive (it doesn't care if you write your statements as all uppercase or lowercase), we discussed some best practices. The order of the key words does matter! Using what you know so far, you will want to write your statements as:
